123460bdab43386272c73590f34ef92d329cb81e
Anna <anna@annaidam.com>
2022-09-26 12:55:25 +0200

    Adding tasks 2 (finished) and 4 (not finished)

---
+++ src/Skill.java
@@ -0,0 +1,49 @@
+public class Skill {
+    /*A pokemon relies on a skill during battles. A skill has a name, attack power (AP), and energy
+    cost (EC). All these values are specified when creating the skill. Both AP and energy cost are
+    integer values. Once created, the state of a skill cannot be changed.*/
+
+    private String name;
+    private int attackPower;
+    private int energyCost;
+
+    Skill(String name, int attackPower, int energyCost) {
+        this.name = name;
+        this.attackPower = attackPower;
+        this.energyCost = energyCost;
+    }
+
+    public String getSKillName() {
+        return this.name;
+    }
+
+    public int getAttackPower() {
+        return this.attackPower;
+    }
+
+    public int getEnergyCost() {
+        return this.energyCost;
+    }
+
+    /*Two skills are equal if they have the same names, APs and energy costs. When printed, a skill
+    should return: “<skill name> - AP: <ap> EC: <ec>”.*/
+    public boolean equals(Object anotherSkill) {
+        if (anotherSkill == null) {
+            return false;
+        } else if(anotherSkill instanceof Skill) {
+            Skill otherSkill = (Skill) anotherSkill;
+            if((this.name.equals(otherSkill.name)) && (this.attackPower == otherSkill.attackPower) && (this.energyCost == otherSkill.energyCost)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        String skill = this.getSKillName() + " - AP: " + this.getAttackPower() + " EC: " + this.getEnergyCost();
+        return skill;
+    }
+}
