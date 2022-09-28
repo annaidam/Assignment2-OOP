c28dfe004da225fc79d8f81e5e4223a8f686556b
Anna <anna@annaidam.com>
2022-09-28 14:49:28 +0200

    Updated code

--- src/assignment2/ItemBag.java
+++ src/assignment2/ItemBag.java
@@ -7,6 +7,7 @@ public class ItemBag {
     The bag also stores a collection of items that begins empty when creating the bag.
     The maximum weight cannot change once the bag is created. You don’t need to write code to check if two bags are equal.*/
     private double maxBagWeight;
+    private double currentBagWeight;
     private ArrayList itemsInBag = new ArrayList();
 
     ItemBag(double maxBagWeight, ArrayList itemsInBag) {
@@ -16,9 +17,15 @@ public class ItemBag {
 
     /*The bag should provide: the current number of items stored, the current weight of the bag,
     and its maximum weight. Other operations are defined below.*/
+    public int getNumberOfItems() {return this.itemsInBag.size();}
+
     public double getCurrentBagWeight() {
-        return this.maxBagWeight;
+        for (int i = 0; i < this.itemsInBag.size(); i++) {
+        }
+        return this.currentBagWeight;
     }
+
+    public double getMaxBagWeight() {return this.maxBagWeight;}
 
     public ArrayList getItemsInBag() {
         /*for (int i = 0; i < this.itemsInBag.size(); i++) {
@@ -32,30 +39,42 @@ public class ItemBag {
     sequence. When adding an item to the bag, the item must be placed in the index where its
     weight is higher than the items after them and lighter than those before (i.e., sorted by
     weight).
-    Note: You don’t need to use Arrays.sort or Collections.sort for solving this task. In fact, using
-    these sorting libraries can make your code inefficient.
     For example:
     [ (P1, 20, 4.5), (P2, 20, 4.5), (P3, 20, 2.2)]
     // after adding (PX, 20, 5.3) - placed at index 0
     [ (PX, 20, 5.3), (P1, 20, 4.5), (P2, 20, 4.5), (P3, 20, 2.2)]
     // after adding (PY, 40, 4.5) - placed at index 1
     [ (PX, 20, 5.3), (PY, 40, 4.5), (P1, 20, 4.5), (P2, 20, 4.5),
-    (P3, 20, 2.2)]
+    (P3, 20, 2.2)]*/
+    public void addItems() {
+        for (int i = 0; i < this.itemsInBag.size(); i++) {
+            itemsInBag.add(i);
+        }
+    }
 
-    7.2 - Removing items:
+    /*7.2 - Removing items:
     The only way to retrieve a reference to an item in the bag is to remove it. Items are removed
     based on a specified index. When removing an item, the bag should automatically
     reorganise itself so that order is preserved and no “empty” slots are left.
     The bag should then return a reference to the removed item. In case the specified index is
-    out of bounds, the method should return null.
+    out of bounds, the method should return null.*/
+    public void removeItems() {
 
-    7.3 - Peeking at items:
+    }
+
+    /*7.3 - Peeking at items:
     A user can peek at the item in a specific position in the bag. The bag should not provide a
     reference to the actual item. Instead, the bag returns a string representation of the item
-    according to what has been specified in Task X.
+    according to what has been specified in Task X.*/
+    public void peekinAtItems() {
 
-    7.4 - Popping items:
+    }
+
+    /*7.4 - Popping items:
     A user can retrieve the heaviest item in the bag. This means that the bag should remove
     and return a reference to its first item. Typically, this operation is named ‘pop’. If the bag is
     empty, popping should return null.*/
+    public void poppingItems() {
+
+    }
 }
