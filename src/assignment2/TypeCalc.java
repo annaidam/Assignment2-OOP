d52eacb62a37ff38e2745469dea9ae1f61ea3c7f
Anna <anna@annaidam.com>
2022-09-28 18:24:56 +0200

    Updated code

---
+++ src/assignment2/TypeCalc.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class TypeCalc {
+
+
+    /*if ((this.getType().equals("Water") && targetPokemon.getType().equals("Fire"))
+            | (this.getType().equals("Fire") && targetPokemon.getType().equals("Grass"))
+            | (this.getType().equals("Grass") && targetPokemon.getType().equals("Water"))){
+                receiveDamage(this.pokemonSkill.getAttackPower() * 2);
+                message = getName() + " uses " + this.pokemonSkill.getSkillName() + " on " + targetPokemon.getName() +
+                        ". It is super effective!\n" + targetPokemon.getName() + " has " + targetPokemon.getCurrentHP() + " HP left.";
+            } else if ((targetPokemon.getType().equals("Water") && this.getType().equals("Grass") | this.getType().equals("Water"))
+            | (targetPokemon.getType().equals("Fire") && this.getType().equals("Water") | this.getType().equals("Fire")
+            | (targetPokemon.getType().equals("Grass") && this.getType().equals("Grass") | this.getType().equals("Fire")) {
+                receiveDamage((this.pokemonSkill.getAttackPower() * 0.5)%2f);
+                message = getName() + " uses " + this.pokemonSkill.getSkillName() + " on " + targetPokemon.getName() +
+                        ". It is not very effective...\n" + targetPokemon.getName() + " has " + targetPokemon.getCurrentHP() + " HP left.";*/
+}
