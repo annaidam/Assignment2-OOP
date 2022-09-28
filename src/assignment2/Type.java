9ba11fa3425a88c3ed6aefd191d644489fdaaee2
Anna <anna@annaidam.com>
2022-09-28 14:58:08 +0200

    Updated code

--- src/assignment2/Type.java
+++ src/assignment2/Type.java
@@ -1,18 +1,5 @@
 package assignment2;
 
-/*For this assignment, we will use four types of pokemon: Water, Fire, Grass and Normal.
-    These types offer advantage for the pokemon’s attack and defence during battle. During
-    attacks, the damage has a multiplier that can significantly increase or reduce the damage
-    value.
-    If the attacker has a type advantage, then the attack is super effective. However, if the target
-    has the advantage, then the attack is not very effective.
-    ● Super effective attacks cause double damage (damage * 2).
-    ● “Not every effective” reduce the damage in half (damage * 0.5).
-    ● Otherwise, the multiplier is the plain damage value (damage * 1).
-    The resulting damage is always rounded down to the closest integer.
-    Recommendations: Check the usage of Enums in Java. Find the right relationships between
-    pokemon and the types, i.e., avoid coupling Pokemon and the type calculations.*/
-
 public enum Type {
     WATER,
     FIRE,
