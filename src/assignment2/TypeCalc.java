f09ace715b3ff6e30c6afb25dfbcd4b4f4df5306
Anna <anna@annaidam.com>
2022-09-29 23:23:03 +0200

    I fixed some small things and added comments to try the task 8 if we have time

--- src/assignment2/TypeCalc.java
+++ src/assignment2/TypeCalc.java
@@ -2,12 +2,26 @@ package assignment2;
 
 // TODO: comment this static Class
 public class TypeCalc {
+    //for this matrix we got help
     final static double[][] EFFECTIVE_MATRIX = {
             {1, 1, 1, 1},
             {1, 0.5, 0.5, 2},
             {1, 2, 0.5, 0.5},
             {1, 0.5, 2, 0.5}
     };
+
+    /* TRYING TASK 8:
+    final static double[][] EFFECTIVE_MATRIX = {
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 0.5, 2, 1, 1},
+            {1, 1, 2, 1, 1, 1, 1, 1},
+            {1, 1, 0.5, 0.5, 1, 0.5, 1, 2}
+            {1, 2, 0.5, 1, 1, 2, 2, 0.5}
+            {1, 0.5, 0.5, 1, 1, 0.5, 1, 2}
+            {1, 1, 2, 1, 1, 2, 1, 0.5}
+            {1, 1, 0.5, 1, 1, 0.5, 1, 0.5}
+    };
+     */
 
     public static double getFactorValue(int rowIndex, int columnIndex) {
         return EFFECTIVE_MATRIX[rowIndex][columnIndex];
