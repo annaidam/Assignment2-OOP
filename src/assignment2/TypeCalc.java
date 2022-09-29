f9788e3f126426ae05856825e856c069ce47661d
isabelamitac <isabelamitac98@gmail.com>
2022-09-29 11:38:23 +0200

    edited code

--- src/assignment2/TypeCalc.java
+++ src/assignment2/TypeCalc.java
@@ -1,7 +1,21 @@
 package assignment2;
 
 public class TypeCalc {
-//damage is double
+
+    public double damageValue;
+
+    public TypeCalc(double damageValue){
+        damageValue=
+
+        if ((this.getType().equals("Water") && targetPokemon.getType().equals("Fire"))
+                | (this.getType().equals("Fire") && targetPokemon.getType().equals("Grass"))
+                | (this.getType().equals("Grass") && targetPokemon.getType().equals("Water"))){
+            damageValue= damageValue* 2;
+
+        if ((targetPokemon.getType().equals("Water") && this.getType().equals("Grass") | this.getType().equals("Water"))
+                | (targetPokemon.getType().equals("Fire") && this.getType().equals("Water") | this.getType().equals("Fire")
+                | (targetPokemon.getType().equals("Grass") && this.getType().equals("Grass") | this.getType().equals("Fire")) {
+            damageValue= (int) damageValue* 0.5;
 
     /*if ((this.getType().equals("Water") && targetPokemon.getType().equals("Fire"))
             | (this.getType().equals("Fire") && targetPokemon.getType().equals("Grass"))
@@ -15,4 +29,5 @@ public class TypeCalc {
                 receiveDamage((this.pokemonSkill.getAttackPower() * 0.5)%2f);
                 message = getName() + " uses " + this.pokemonSkill.getSkillName() + " on " + targetPokemon.getName() +
                         ". It is not very effective...\n" + targetPokemon.getName() + " has " + targetPokemon.getCurrentHP() + " HP left.";*/
+}
 }
