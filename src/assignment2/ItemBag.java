1cc5b4bf2e99e337774ae8d3391cc51cbd90a528
isabelamitac <isabelamitac98@gmail.com>
2022-09-30 18:34:26 +0200

    Changes by annaidam

--- src/assignment2/ItemBag.java
+++ src/assignment2/ItemBag.java
@@ -1,5 +1,6 @@
 package assignment2;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ItemBag {
@@ -16,13 +17,20 @@ public class ItemBag {
 
     /*The bag should provide: the current number of items stored, the current weight of the bag,
     and its maximum weight. Other operations are defined below.*/
-    public int getNumOfItems() {return this.itemBag.size();}
+    public int getNumOfItems() {
+        int sizeOfBag = 0;
+        if (this.itemBag.isEmpty()) {
+            return sizeOfBag;
+        } else {
+            return sizeOfBag = this.itemBag.size();
+        }
+    }
 
     public double getCurrentWeight() {
         //find the weight of each item and sum them up
         if (!this.itemBag.isEmpty()) {
             for (Item currentItem : itemBag) {
-                currentBagWeight = (currentBagWeight + currentItem.getItemWeight()%2f);
+                currentBagWeight = (currentBagWeight + currentItem.getItemWeight());
             }
         }
         return this.currentBagWeight;
@@ -30,32 +38,97 @@ public class ItemBag {
 
     public double getMaxWeight() {return this.maxBagWeight;}
 
+    public void sortTheBag() {
+        if (!this.itemBag.isEmpty()) {
+            int length = itemBag.size();
+            if (length > 1) {
+                for (int i = 0; i < length; i++) {
+                    Item currentItem1 = this.itemBag.get(i);
+                    for (int j = i + 1; j < length; j++) {
+                        Item currentItem2 = this.itemBag.get(j);
+                        if (currentItem1.getItemWeight() < currentItem2.getItemWeight()) {
+                            Item x = currentItem1;
+                            currentItem1 = currentItem2;
+                            currentItem2 = x;
+                            //itemBag.add(itemBag.indexOf(currentItem1), currentItem2);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+/*
+    public void sortTheBag() {
+        for (int i = 0; i < this.itemBag.size(); i++) {
+            Item currentItem1 = itemBag.get(i);
+            for (int j = i + 1; j < this.itemBag.size(); j++) {
+                Item currentItem2 = itemBag.get(j);
+                Item newItem;
+                Item oldItem;
+                if (currentItem1.getItemWeight() < currentItem2.getItemWeight()) {
+                    x = this.oldItem(i);
+                    this.oldItem(i) = this.newItem(j);
+                    this.oldItem(j) = x;
+                }
+            }
+        }
+    }
+
+
+ */
+
     /*7.1 - Adding items to the bag:
     The collection of items can accept repeated items and the items are stored in a specific
     sequence. When adding an item to the bag, the item must be placed in the index where its
     weight is higher than the items after them and lighter than those before (i.e., sorted by
-    weight).
-    For example:
-    [ (P1, 20, 4.5), (P2, 20, 4.5), (P3, 20, 2.2)]
-    // after adding (PX, 20, 5.3) - placed at index 0
-    [ (PX, 20, 5.3), (P1, 20, 4.5), (P2, 20, 4.5), (P3, 20, 2.2)]
-    // after adding (PY, 40, 4.5) - placed at index 1
-    [ (PX, 20, 5.3), (PY, 40, 4.5), (P1, 20, 4.5), (P2, 20, 4.5),
-    (P3, 20, 2.2)]*/
+    weight).*/
+/*
+ Pseudocode :
+PROCEDURE LINEAR_SEARCH (LIST, VALUE)
+  FOR EACH ITEM IN THE LIST
+     IF SAME ITEM == VALUE
+          RETURN THE ITEM’S LOCATION
+     END IF
+  END FOR
+END PROCEDURE*/
+
     public int addItem(Item newItem) {
-        for (int i = 0; i < this.itemBag.size(); i++) {
-            Item currentItem = itemBag.get(i);
-            //Item heaviestItem = itemBag.get(0);
-            if (currentItem.getItemWeight() > newItem.getItemWeight()) {
-                //heaviestItem = currentItem;
-                itemBag.add(i + 1, newItem);
-            } else if (currentItem.getItemWeight() < newItem.getItemWeight()) {
-                itemBag.add(i - 1, newItem);
+        if (itemBag.isEmpty()) {
+            itemBag.add(0, newItem);
+            return 0;
+        } else {
+            for (int i = 0; i < itemBag.size(); i++) {
+                Item currentItem = itemBag.get(i);
+                if (currentItem.getItemWeight() < newItem.getItemWeight()) {
+                    itemBag.add(itemBag.indexOf(currentItem), newItem);
+                }
             }
         }
-        int index = itemBag.indexOf(newItem);
-        return index;
+        int indexOfNewItem = itemBag.indexOf(newItem);
+        return indexOfNewItem;
     }
+
+    /*public int addItem(Item newItem) {
+        sortTheBag();
+        int indexOfNewItem;
+        int length = itemBag.size();
+        for (int i = 0; i < length; i++) {
+            Item oldItem = itemBag.get(i);
+            for (int j = i + 1; j < length; j++) {
+                newItem = itemBag.get(j);
+                //itemBag.add(j, newItem);
+                if (oldItem.getItemWeight() < newItem.getItemWeight()) {
+                    oldItem = itemBag.get(i);
+                    itemBag.add(j, oldItem);
+                    itemBag.add(i, newItem);
+                    indexOfNewItem = itemBag.indexOf(newItem);
+                }
+            }
+        }
+        sortTheBag();
+        return indexOfNewItem;
+    }*/
 
     /*7.2 - Removing items:
     The only way to retrieve a reference to an item in the bag is to remove it. Items are removed
@@ -67,10 +140,8 @@ public class ItemBag {
     public String removeItemAt(int index) {
         //remove an item based on specified index
         //automatically reorganise the items in the bag
-        String removedItem = null;
-        Item heaviestItem = itemBag.get(0);
+        String removing = "";
         for (int i = 0; i < this.itemBag.size(); i++) {
-            Item currentItem = itemBag.get(i);
             if (index == i) {
                 Item removedItem = itemBag.get(i);
                 removing = removedItem.toString();
@@ -79,20 +150,9 @@ public class ItemBag {
                 removing = null;
             }
         }
-        return removedItem = null;
+        sortTheBag();
+        return removing;
     }
-
-    /*
-    public reOrderTheBag() {
-        for (int i = 0; i < this.itemBag.size(); i++) {
-            Item currentItem = itemBag.get(i);
-            Item heaviestItem = itemBag.get(0);
-            if (currentItem.getItemWeight() > heaviestItem.getItemWeight()) {
-            heaviestItem = currentItem;
-                    }
-                }
-            }
-     */
 
     /*7.3 - Peeking at items:
     A user can peek at the item in a specific position in the bag. The bag should not provide a
