c1912854981b15408b4e9ff6f7ad5c23e4ec811f
isabelamitac <isabelamitac98@gmail.com>
2022-10-01 13:36:15 +0200

    tried another approach, but still not working

--- src/assignment2/ItemBag.java
+++ src/assignment2/ItemBag.java
@@ -43,83 +43,75 @@ public class ItemBag {
     sequence. When adding an item to the bag, the item must be placed in the index where its
     weight is higher than the items after them and lighter than those before (i.e., sorted by
     weight).*/
-/*
- Pseudocode :
-PROCEDURE LINEAR_SEARCH (LIST, VALUE)
-  FOR EACH ITEM IN THE LIST
-     IF SAME ITEM == VALUE
-          RETURN THE ITEM’S LOCATION
-     END IF
-  END FOR
-END PROCEDURE*/
 
-
-    public int addItem(Item newItem) {
-        //adding a new item
-        itemBag.add(newItem);
-        //if bag is empty, we place the item in the first slot
-        if (this.itemBag.isEmpty()) {
-            itemBag.set(0, newItem);
-        //if after adding the item, the bag's weight exceeds the max bag weight,
-        //we remove the added item and return its index BUT because in the example
-        //the removed item is equal to other two items added, this will get the index
-        //of the equal item, not the item we removed
-        } else if (this.currentBagWeight >= this.maxBagWeight) {
-            itemBag.remove(newItem);
-            return itemBag.indexOf(newItem);
-        //after adding the item, we calculate its right place
-        } else {
-            int length = itemBag.size();
-            for (int i = 0; i < length; i++) {
-                for (int j = length - 1; j > i; j--) {
-                    if (itemBag.get(i).getItemWeight() < itemBag.get(j).getItemWeight()) {
-                        Item temporary = itemBag.get(i);
-                        itemBag.set(i, itemBag.get(j));
-                        itemBag.set(j, temporary);
-                    }
-                }
-            }
-        }
-        return itemBag.indexOf(newItem);
-    }
     /*
-    public int addItem(Item newItem) {
-        if (itemBag.isEmpty()) {
-            itemBag.add(0, newItem);
-            return 0;
-        } else {
-            for (int i = 0; i < itemBag.size(); i++) {
-                Item currentItem = itemBag.get(i);
-                if (currentItem.getItemWeight() < newItem.getItemWeight()) {
-                    itemBag.add(itemBag.indexOf(currentItem), newItem);
-                }
-            }
-        }
-        int indexOfNewItem = itemBag.indexOf(newItem);
-        return indexOfNewItem;
-    }
+       public int addItem(Item newItem) {
+           //adding a new item
+           itemBag.add(newItem);
+           //if bag is empty, we place the item in the first slot
+           if (this.itemBag.isEmpty()) {
+               itemBag.set(0, newItem);
+           //if after adding the item, the bag's weight exceeds the max bag weight,
+           //we remove the added item and return its index BUT because in the example
+           //the removed item is equal to other two items added, this will get the index
+           //of the equal item, not the item we removed
+           } else if (this.currentBagWeight >= this.maxBagWeight) {
+               itemBag.remove(newItem);
+               return itemBag.indexOf(newItem);
+           //after adding the item, we calculate its right place
+           } else {
+               int length = itemBag.size();
+               for (int i = 0; i < length; i++) {
+                   for (int j = length - 1; j > i; j--) {
+                       if (itemBag.get(i).getItemWeight() < itemBag.get(j).getItemWeight()) {
+                           Item temporary = itemBag.get(i);
+                           itemBag.set(i, itemBag.get(j));
+                           itemBag.set(j, temporary);
+                       }
+                   }
+               }
+           }
 
-*/
-    /*public int addItem(Item newItem) {
-        sortTheBag();
-        int indexOfNewItem;
-        int length = itemBag.size();
-        for (int i = 0; i < length; i++) {
-            Item oldItem = itemBag.get(i);
-            for (int j = i + 1; j < length; j++) {
-                newItem = itemBag.get(j);
-                //itemBag.add(j, newItem);
-                if (oldItem.getItemWeight() < newItem.getItemWeight()) {
-                    oldItem = itemBag.get(i);
-                    itemBag.add(j, oldItem);
-                    itemBag.add(i, newItem);
-                    indexOfNewItem = itemBag.indexOf(newItem);
-                }
-            }
+           return itemBag.indexOf(newItem);
+       }
+           /*
+
+     We start with an ArrayList of length 0, therein, we assign the item to the first, that is, the only available position.
+     That’s the base case.
+
+       For the general case: we may consider the traditional linear search - we iterate over the List.
+       We check whether the value of the current item,
+       i. e., the weight is greater than the value (weight of the item) at the i-th index,
+       if so, we insert the current item at that certain index,
+       using the in-built behaviours of the ArrayList Class - “add(index, item)”,
+       therefore enlarging the List by +1.
+       That’s the whole idea of the algorithm of time complexity O(n).
+
+       2 extra points to consider:
+   - ensure that the maximum weight isn’t exceeded;
+   - ensure that the item will increase the weight stored in the item bag.
+        */
+        public int addItem(Item newItem) {
+            if (this.itemBag.isEmpty()) {
+                itemBag.add(newItem);
+
+            int length = itemBag.size();
+            if(this.currentBagWeight < this.maxBagWeight){
+            for (int i = 0; i < length; i++)
+                if(newItem.getItemWeight() > itemBag.get(i).getItemWeight()){
+                //if(current item weight > item weight at index i)
+                    //add(index i, current item)
+
+                itemBag.add(itemBag.indexOf(itemBag.get(i)), newItem);
+                length = length + 1;
+                this.currentBagWeight = this.currentBagWeight + newItem.getItemWeight();
+
         }
-        sortTheBag();
-        return indexOfNewItem;
-    }*/
+        }
+            }
+            return itemBag.indexOf(newItem);
+        }
+
 
     /*7.2 - Removing items:
     The only way to retrieve a reference to an item in the bag is to remove it. Items are removed
@@ -128,6 +120,8 @@ END PROCEDURE*/
 
     The bag should then return a reference to the removed item. In case the specified index is
     out of bounds, the method should return null.*/
+
+
     public String removeItemAt(int index) {
         //remove an item based on specified index
         //automatically reorganise the items in the bag
