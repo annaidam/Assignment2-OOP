82a6f7056fd9a13e9d208583b8b201caee7cccb9
isabelamitac <isabelamitac98@gmail.com>
2022-10-02 19:38:49 +0200

    Changes by annaidam

--- src/assignment2/TypeCalc.java
+++ src/assignment2/TypeCalc.java
@@ -2,14 +2,14 @@ package assignment2;
 
 public class TypeCalc {
     //for this matrix we got help
-    final static double[][] EFFECTIVE_MATRIX = {
+    /*final static double[][] EFFECTIVE_MATRIX = {
             {1, 1, 1, 1},
             {1, 0.5, 0.5, 2},
             {1, 2, 0.5, 0.5},
             {1, 0.5, 2, 0.5}
-    };
+    };*/
 
-    /*final static double[][] EFFECTIVE_MATRIX = {
+    final static double[][] EFFECTIVE_MATRIX = {
             {1, 1, 1, 1, 1, 1, 1, 1},
             {1, 1, 1, 1, 0.5, 2, 1, 1},
             {1, 1, 2, 1, 1, 1, 1, 1},
@@ -18,7 +18,7 @@ public class TypeCalc {
             {1, 0.5, 0.5, 1, 1, 0.5, 1, 2},
             {1, 1, 2, 1, 1, 2, 1, 0.5},
             {1, 1, 0.5, 1, 1, 0.5, 1, 0.5}
-    };*/
+    };
 
     public static double getFactorValue(int rowIndex, int columnIndex) {
         return EFFECTIVE_MATRIX[rowIndex][columnIndex];
